65cb66e99f2f8567ec0252e192b883f6e3321b09
haohmaru2010 <tom.ngo.cs@gmail.com>
2020-10-05 17:29:02 +1100

    Add fonts

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 
 import './index.css';
+import './fonts/OpenSansCondensed-Light.ttf'
 import App from './App';
 
 ReactDOM.render(
